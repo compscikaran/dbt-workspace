9be2173707075cf4862487dcf3dcbb704e2c8b98
Karan Gupta <gkaran184@gmail.com>
2025-10-04 14:05:17 +0000

    remove extra files

--- jaffle_shop/stg_default__movies.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    movie_id,
-    title as movie_name,
-    content_type,
-    COALESCE(genre_secondary, genre_primary) AS movie_genre,
-    ROUND(duration_minutes / 60, 1) AS movie_duration,
-    language,
-    is_netflix_original
-FROM analytics.default.movies
